4f44eadd163424331f9e48994dac3d57480b5684
ZinC0508 <18335432463@163.com>
2025-05-25 21:03:42 +0800

    basic movement

--- Scripts/Chara.cs
+++ Scripts/Chara.cs
@@ -3,11 +3,30 @@ using System;
 
 public partial class Chara : CharacterBody2D
 {
+	bool isMoving = false;
+	Vector2 targetPosition = Vector2.Zero;
+	float speed = 10f;
 	public override void _Ready()
 	{
-		base._Ready();
+		targetPosition = Position;
 	}
 
+	public override void _PhysicsProcess(double delta)
+	{
+		if (isMoving && (targetPosition - Position).Length() > speed)
+		{
+			Velocity = (targetPosition - Position) / (targetPosition - Position).Length() * speed;
+			Velocity = new Vector2(Velocity.X, 0);
+			MoveAndCollide(Velocity);
+			// MoveAndSlide();
+			// MoveAndCollide((targetPosition - Position) / (targetPosition - Position).Length() * speed);
+		}
+		else
+		{
+			isMoving = false;
+		}
+
+	}
 	public override void _Input(InputEvent @event)
 	{
 		if (@event is InputEventMouseButton mouseEvent)
@@ -15,10 +34,11 @@ public partial class Chara : CharacterBody2D
 			if (mouseEvent.ButtonIndex == MouseButton.Right && mouseEvent.IsPressed())
 			{
 				GD.Print("Right Mouse Button Pressed");
-				if (IsOnFloor())
+				if (IsOnFloor() || true)
 				{
-					MoveAndCollide(new Vector2(100, 0));
-					GD.Print("move");
+					isMoving = true;
+					targetPosition = GetGlobalMousePosition();
+					GD.Print($"from {Position} to {targetPosition}");
 				}
 			}
 		}
---
+++ Scripts/StartGame.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class StartGame : Button
+{
+	public void onPressed()
+	{
+		GetTree().ChangeSceneToFile("res://Scenes/main.tscn");
+	}
+}
