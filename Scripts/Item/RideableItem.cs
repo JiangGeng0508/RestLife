9a70fc2358f60ec4ee846e7f9d94342b02b084a4
丿丨_丨丿 <why3305325552@gmail.com>
2025-05-27 22:22:28 +0800

    rideable item fix

--- Scripts/Item/RideableItem.cs
+++ Scripts/Item/RideableItem.cs
@@ -6,12 +6,20 @@ public partial class RideableItem : InteractableItem
 	[Export] public Vector2 riderOffset { get; set; } = new Vector2(0, -20);
 	private Vector2 _lastPosition;
 	Character rider;
+	bool toggle = false;
+
 	public override void Action()
 	{
-		if (CharaReachArea != null)
+		toggle = !toggle;
+		if (CharaReachArea != null && toggle)
 		{
 			rider = CharaReachArea.GetParent() as Character;
 			rider.Ride(this);
+		}
+		else if(rider != null && !toggle)
+		{
+			rider.StopRiding();
+			rider = null;
 		}
 	}
 }
