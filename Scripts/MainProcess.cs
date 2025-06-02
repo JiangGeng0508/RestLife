971a119d824be6eee46432bfa61a63527f71d0ca
ZinC0508 <18335432463@163.com>
2025-06-02 23:05:52 +0800

    MainProcess

---
+++ Scripts/MainProcess.cs
@@ -0,0 +1,6 @@
+using Godot;
+using System;
+
+public partial class MainProcess : Node2D
+{
+}
--- Scripts/UI/GodMode.cs
+++ Scripts/UI/GodMode.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class GodMode : ItemList
+public partial class GodMode : VBoxContainer
 {
 	[Export]
 	public bool isDebug { get; set; } = false;
@@ -10,10 +10,25 @@ public partial class GodMode : ItemList
 	{
 		if (!isDebug) return;
 		AddDebugSelection("DebugPrint", () => { GD.Print("Debug"); });
-		AddDebugSelection("Pause", () => { GetTree().Paused = true; });
+		// AddDebugSelection("Pause", () => { GetTree().Paused = !GetTree().Paused; });
 	}
 	public void AddDebugSelection(string name, Action action)
 	{
+		Button button = new Button() { Text = name };
+		button.Pressed += action;
+		button.Position = new Vector2(10, 10 + GetChildCount() * 30);
+		button.Size = new Vector2(100, 20);
+		AddChild(button);
+	}
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventKey eventKey && eventKey.Pressed && eventKey.Keycode == Key.F1)
+		{
+			if (isDebug)
+			{
+				Visible = !Visible;
+			}
+		}
 		
 	}
 }
