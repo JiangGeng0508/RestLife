b56f82de60cd63ea87771a45d619822f64a16dcf
ZinC0508 <18335432463@163.com>
2025-06-10 23:16:57 +0800

    bug fix

--- Scripts/Buildings/ItemSelectSlot.cs
+++ Scripts/Buildings/ItemSelectSlot.cs
@@ -5,6 +5,7 @@ using System;
 public partial class ItemSelectSlot : MenuButton
 {
 	public Item SettedItem;
+	Texture2D DefaultIcon = GD.Load<Texture2D>("res://Asset/Sprite/Icon/control/icon_interrogation.png");
 	public void OnPressed()
 	{
 		var popup = GetPopup();
@@ -12,23 +13,27 @@ public partial class ItemSelectSlot : MenuButton
 		Dictionary<int, Item> items = [];
 		popup.Clear();
 		popup.AddItem("None", 0);
+		items[0] = new Item() { Name = "None" };
 		foreach (var item in Global.Player.Inventory.Items)
 		{
-			popup.AddItem(item.Value.Name, ID);
+			popup.AddItem(item.Value.Name + "(" + item.Value.Number + ")", ID);
 			items[ID] = item.Value;
 			ID++;
-			
+
 		}
 		popup.IdPressed += (id) =>
 		{
+			if (SettedItem != null)
+			{
+				Global.Player.Inventory.AddItem(SettedItem, SettedItem.Number);
+			}
 			if (id == 0)
 			{
-				Global.Player.Inventory.AddItem(SettedItem);
 				SettedItem = null;
-				Icon = null;
+				Icon = DefaultIcon;
 				return;
 			}
-			Global.Player.Inventory.RemoveItem(items[(int)id], 1);
+			Global.Player.Inventory.RemoveItem(items[(int)id]);
 			SettedItem = items[(int)id];
 			Icon = items[(int)id].Icon;
 		};
