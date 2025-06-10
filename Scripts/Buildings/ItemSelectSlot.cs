3ce85b884481c44a4fb742f095e8024c39994632
ZinC0508 <18335432463@163.com>
2025-06-10 23:02:13 +0800

    bug

--- Scripts/Buildings/ItemSelectSlot.cs
+++ Scripts/Buildings/ItemSelectSlot.cs
@@ -1,15 +1,36 @@
 using Godot;
+using Godot.Collections;
 using System;
 
 public partial class ItemSelectSlot : MenuButton
 {
+	public Item SettedItem;
 	public void OnPressed()
 	{
 		var popup = GetPopup();
+		var ID = 1;
+		Dictionary<int, Item> items = [];
 		popup.Clear();
+		popup.AddItem("None", 0);
 		foreach (var item in Global.Player.Inventory.Items)
 		{
-			popup.AddItem(item.Value.Name);
+			popup.AddItem(item.Value.Name, ID);
+			items[ID] = item.Value;
+			ID++;
+			
 		}
+		popup.IdPressed += (id) =>
+		{
+			if (id == 0)
+			{
+				Global.Player.Inventory.AddItem(SettedItem);
+				SettedItem = null;
+				Icon = null;
+				return;
+			}
+			Global.Player.Inventory.RemoveItem(items[(int)id], 1);
+			SettedItem = items[(int)id];
+			Icon = items[(int)id].Icon;
+		};
 	}
 }
