ab8904c65165066754c813947d95c69d5a39dd8b
丿丨_丨丿 <why3305325552@gmail.com>
2025-06-15 22:21:48 +0800

    menu esc close

--- Scripts/UI/Hud/SelectMenu.cs
+++ Scripts/UI/Hud/SelectMenu.cs
@@ -5,13 +5,23 @@ public partial class SelectMenu : TabContainer
 {
 	public override void _Ready()
 	{
+		Global.SelectMenu = this;
 		Visible = false;
 	}
 	public override void _Input(InputEvent @event)
 	{
-		if (@event is InputEventKey eventKey && eventKey.Pressed && eventKey.Keycode == Key.Tab)
+		if (@event is InputEventKey eventKey && eventKey.Pressed)
 		{
-			Visible = !Visible;
+			if (eventKey.Keycode == Key.Tab)
+			{
+				Visible = !Visible;
+				Global.Player.Character.OnToggleMenu();
+			}
+			else if (Visible && eventKey.Keycode == Key.Escape)
+			{
+				Visible = false;
+				Global.Player.Character.OnToggleMenu();
+			}
 		}
 	}
 }
