d24b34c0f38496417fa68a96e7e80344ad2ad7fa
ZinC0508 <18335432463@163.com>
2025-07-07 15:10:56 +0800

    recipe

--- Scripts/UI/RecipeManager.cs
+++ Scripts/UI/RecipeManager.cs
@@ -10,15 +10,44 @@ public static class RecipeManager
 	{
 		using var dir = DirAccess.Open("res://Asset/Data/Recipes/");
 		var files = dir.GetFiles();
+		var count = 0;
 		foreach (var file in files)
 		{
 			GD.Print("Loading Recipe: " + file);
 			var recipe = GD.Load<Recipe>("res://Asset/Data/Recipes/" + file);
+			GD.Print($"{recipe.Ingredients[0].Name} -> {recipe.Result.Name}");
 			RegisterRecipe(recipe);
+			count++;
 		}
+		GD.Print("Loaded " + count + " recipes");
 	}
 	public static void RegisterRecipe(Recipe recipe)
 	{
 		RegisteredRecipes.Add(recipe);
+	}
+	public static Item CheckRecipe(Item[] ingredients)
+	{
+		foreach (var recipe in RegisteredRecipes)
+		{
+			if (recipe.Ingredients.Length == ingredients.Length)
+			{
+				bool match = true;
+				for (int i = 0; i < recipe.Ingredients.Length; i++)
+				{
+					GD.Print("Checking " + ingredients[i].Name);
+					if (recipe.Ingredients[i] != ingredients[i])
+					{
+						match = false;
+						break;
+					}
+				}
+				if (match)
+				{
+					return recipe.Result;
+				}
+			}
+		}
+		GD.PrintErr("No recipe found for ingredients");
+		return null;
 	}
 }
