53d11cb16b499269ed470e65d20f12e1334d1163
ZinC0508 <18335432463@163.com>
2025-07-07 11:49:02 +0800

    position fix

--- Scripts/UI/Hud/NeoCauldron.cs
+++ Scripts/UI/Hud/NeoCauldron.cs
@@ -6,8 +6,8 @@ public partial class NeoCauldron : Window
 	public override void _Ready()
 	{
 		CloseRequested += Hide;
-		AddChild(new DragSlotButton());
-		AddChild(new DragSlotButton());
-		AddChild(new DragSlotButton());
+		AddChild(new DragSlotButton(){ Position = new Vector2(10,0) });
+		AddChild(new DragSlotButton(){ Position = new Vector2(60,0) });
+		AddChild(new DragSlotButton(){ Position = new Vector2(110,0) });
 	}
 }
